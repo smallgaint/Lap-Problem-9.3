6a0ca7100674252595758727175b8a680bccd409
Sungrae Hong <s-hong.1@onu.edu>
2025-10-30 13:02:00 -0400

    completed hasRun

--- p9-3/p9-3.cpp
+++ p9-3/p9-3.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 
 void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
 
 int main() {
 	const int SIZE = 10;
-	int array[SIZE] = {5, 5, 5, 20, 25, 30, 30, 40, 45, 45};
-	displayRun(array, SIZE);
+	int array[SIZE] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 55};
+	if(hasRun(array, SIZE)) {
+		displayRun(array, SIZE);
+	} else {
+		cout << "The array does not have a run." <<  endl;
+	}
+	
 	return 0;
 }
 
@@ -26,4 +32,13 @@ void displayRun(int values[], int size) {
 		}
 		cout << " ";
 	}
+}
+
+bool hasRun(int values[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		if (values[i] == values[i + 1]) {
+			return true;
+		}
+	}
+	return false;
 }
